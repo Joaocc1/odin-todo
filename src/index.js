8d41107247b6c760118ab3cfe60bb9d87670f95a
Joao Colaco <114249810+Joaocc1@users.noreply.github.com>
2026-07-17 11:31:03 +0100

    refactor: date is now formatted and id is randomly generated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
+import { format } from "date-fns"
+
 // create an array that will be the default project for todos
 const mainProject = []
 
 // create a class that creates more projects
 class Todo {
 
-  constructor(title, description, date, label, priority, id) {
+  constructor(title, description, date, label, priority) {
     this.title = title;
     this.description = description
-    this.date = date
+    this.date = format(new Date(date), "dd/MM/yyyy")
     this.label = label
     this.priority = priority
-    this.id = id
-    // will need to take consideration at date an id as they are generated
+    this.id = crypto.randomUUID()
+
 
   }
 }
 
 // testing generating todos using class
-const todo = new Todo("title test", "this is what I have to do", "dev", "today", 1, 1)
+const todo = new Todo("title test", "this is what I have to do", "2026, 01, 08", "dev", 1)
 mainProject.push(todo)
 
 console.log(mainProject)
