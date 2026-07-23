70afedfc8e6ef600d643705f1b3a7a45c2f6620b
Joao Colaco <114249810+Joaocc1@users.noreply.github.com>
2026-07-23 11:35:43 +0100

    comment next steps

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import App from "./App.js";
 
 // ++++++++++ Next steps +++++++++++++
 //
+// 1. Change todos to not be inside arrays of each project but just assign them a project
+// 2. add a change project to todo class
 // 1. implement ui
 // 2. implement local storage
 //
