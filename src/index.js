077fd06301372d43a5ad92a59b462cd2bbca29ab
Joao Colaco <114249810+Joaocc1@users.noreply.github.com>
2026-08-08 17:17:48 +0100

    finish testing function that generates an html element

--- src/index.js
+++ src/index.js
@@ -8,10 +8,35 @@ console.log(getProjects())
 getTodo(1).editTitle("New title")
 console.log(getTodos())
 
+// ********** testing ui generation *********
+
+function newElement({type, parent, addClass, text, attributes = []} = {}) {
+  const parentNode = document.querySelector(parent)
+  const newElement = document.createElement(type)
+  newElement.classList.add(addClass)
+
+  if (text && text !== "") {
+    newElement.textContent = text
+  }
+
+  if (attributes.length !== 0) {
+    for (let i = 0; i < attributes.length; i++) {
+      newElement.setAttribute(attributes[i][0], attributes[i][1])
+    }
+  }
+
+  parentNode.appendChild(newElement)
+}
+
+import image from "./assets/add.png"
+
+newElement({ type: "div", parent: "#content", addClass: "header" })
+newElement({type: "h1", parent: ".header", text: "Hello World"})
+
+newElement({ type: "img", parent: ".header", attributes: [["src", image]] })
+
 // ++++++++++ Next steps +++++++++++++
 //
-// 1. Change todos to not be inside arrays of each project but just assign them a project
-// 2. add a change project to todo class
 // 1. implement ui
 // 2. implement local storage
 //
